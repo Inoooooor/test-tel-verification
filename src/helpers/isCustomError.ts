b6b8f38307d1c8b8ae94f49814420b6fd42130ee
Inoooooor <111541493+Inoooooor@users.noreply.github.com>
2024-10-18 10:50:48 +0300

    Update isCustomError.ts

--- src/helpers/isCustomError.ts
+++ src/helpers/isCustomError.ts
@@ -1,5 +1,6 @@
-export default function isCustomError(
-  error: unknown,
-): error is { status?: number } {
+export default function isCustomError(error: unknown): error is {
+  response: any
+  status?: number
+} {
   return typeof error === 'object' && error !== null && 'status' in error
 }
